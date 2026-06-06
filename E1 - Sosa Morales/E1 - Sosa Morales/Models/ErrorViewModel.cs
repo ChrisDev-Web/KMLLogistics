d21be5b2fbaa2620e551eecf31a4b5015bc8e9f8
Chris Dev <chris.sosa.dev.23@gmail.com>
2026-06-06 10:26:41 -0500

    Inicializando Proyecto C#

---
+++ E1 - Sosa Morales/E1 - Sosa Morales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E1___Sosa_Morales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
